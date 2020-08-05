085912f0e0017ec22aaf752d68d99919f724f209
thuantran73 <larten1234@gmail.com>
2020-08-05 12:19:27 +0700

    Quản lí nhân viên và hóa đơn

---
+++ vluCanteenAdmin/vluCanteenAdmin/Controllers/BillsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using vluCanteenAdmin.Models;
+
+namespace vluCanteenAdmin.Controllers
+{
+    public class BillsController : Controller
+    {
+        private SEP23Team9Entities1 db = new SEP23Team9Entities1();
+
+        // GET: Bills
+        public ActionResult Index()
+        {
+            var bills = db.Bills.Include(b => b.Food1).Include(b => b.User);
+            return View(bills.ToList());
+        }
+
+        // GET: Bills/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // GET: Bills/Create
+        public ActionResult Create()
+        {
+            ViewBag.Food_ID = new SelectList(db.Food1, "Food_ID", "Food_Name");
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Email");
+            return View();
+        }
+
+        // POST: Bills/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Bill_ID,Food_ID,User_ID,Time,Date,Price,Amount")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bills.Add(bill);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Food_ID = new SelectList(db.Food1, "Food_ID", "Food_Name", bill.Food_ID);
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Email", bill.User_ID);
+            return View(bill);
+        }
+
+        // GET: Bills/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Food_ID = new SelectList(db.Food1, "Food_ID", "Food_Name", bill.Food_ID);
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Email", bill.User_ID);
+            return View(bill);
+        }
+
+        // POST: Bills/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Bill_ID,Food_ID,User_ID,Time,Date,Price,Amount")] Bill bill)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bill).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Food_ID = new SelectList(db.Food1, "Food_ID", "Food_Name", bill.Food_ID);
+            ViewBag.User_ID = new SelectList(db.Users, "User_ID", "Email", bill.User_ID);
+            return View(bill);
+        }
+
+        // GET: Bills/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bill bill = db.Bills.Find(id);
+            if (bill == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bill);
+        }
+
+        // POST: Bills/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bill bill = db.Bills.Find(id);
+            db.Bills.Remove(bill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ vluCanteenAdmin/vluCanteenAdmin/Controllers/UsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using vluCanteenAdmin.Models;
+
+namespace vluCanteenAdmin.Controllers
+{
+    public class UsersController : Controller
+    {
+        private SEP23Team9Entities1 db = new SEP23Team9Entities1();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var users = db.Users.Include(u => u.UserType);
+            return View(users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.Type_ID = new SelectList(db.UserTypes, "Type_ID", "User_Type");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "User_ID,Email,Name,Type_ID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Type_ID = new SelectList(db.UserTypes, "Type_ID", "User_Type", user.Type_ID);
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Type_ID = new SelectList(db.UserTypes, "Type_ID", "User_Type", user.Type_ID);
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "User_ID,Email,Name,Type_ID")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Type_ID = new SelectList(db.UserTypes, "Type_ID", "User_Type", user.Type_ID);
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
